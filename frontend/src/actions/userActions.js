6f09881b9288542a04ae22a23c9ddd7c6f89ac53
sMathujan <sivananthanmathujan@gmail.com>
2021-06-07 21:28:51 +0530

    Update User Functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,7 +4,8 @@ import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT,
          USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_DETAILS_RESET,
          USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_RESET, 
          USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_LIST_FAIL, USER_LIST_RESET, 
-         USER_DELETE_REQUEST, USER_DELETE_SUCCESS, USER_DELETE_FAIL,
+         USER_DELETE_REQUEST, USER_DELETE_SUCCESS, USER_DELETE_FAIL, 
+         USER_UPDATE_REQUEST, USER_UPDATE_SUCCESS, USER_UPDATE_FAIL,
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
@@ -230,6 +231,45 @@ export const login = (email, password) => async (dispatch) => {
           error.response && error.response.data.message
             ? error.response.data.message
             : error.message,
+      })
+    }
+  }
+
+  export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: USER_UPDATE_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.put(`/api/users/${user._id}`, user, config)
+  
+      dispatch({ type: USER_UPDATE_SUCCESS })
+  
+      dispatch({ type: USER_DETAILS_SUCCESS, payload: data })
+  
+      dispatch({ type: USER_DETAILS_RESET })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: USER_UPDATE_FAIL,
+        payload: message,
       })
     }
   }
